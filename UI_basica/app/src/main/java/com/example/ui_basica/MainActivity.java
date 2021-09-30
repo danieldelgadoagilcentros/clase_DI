9d601dda5dfda1f861d449a69ee7551be2219dd4
danieldelgadoagilcentros <daniel.delgado@agilcentros.es>
2021-09-30 10:56:55 +0200

    Activacion y desactivacion de botones

--- UI_basica/app/src/main/java/com/example/ui_basica/MainActivity.java
+++ UI_basica/app/src/main/java/com/example/ui_basica/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.ui_basica;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.Serializable;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,6 +21,8 @@ public class MainActivity extends AppCompatActivity {
 
     EditText nombre, apellido, telefono;
     Button izda, dcha, guardar;
+    Button topIzda, topDcha, borrar;
+    private boolean borrable;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,6 +45,16 @@ public class MainActivity extends AppCompatActivity {
         dcha = (Button) findViewById(R.id.dcha);
         guardar = (Button) findViewById(R.id.guardar);
 
+        topIzda = (Button) findViewById(R.id.topIzda);
+        topDcha = (Button) findViewById(R.id.topDcha);
+        borrar = (Button) findViewById(R.id.borrar);
+
+        nombre.addTextChangedListener(new myTextChangedListener());
+        apellido.addTextChangedListener(new myTextChangedListener());
+        telefono.addTextChangedListener(new myTextChangedListener());
+
+        izda.OnLongClickListener(new myLongClickIzda());
+
         actualizaUI();
     }
 
@@ -57,6 +73,11 @@ public class MainActivity extends AppCompatActivity {
 
         izda.setEnabled(indice!=0);
         dcha.setEnabled(indice< listaAgenda.size());
+        topIzda.setEnabled(indice!=0);
+        topDcha.setEnabled(indice< listaAgenda.size());
+        borrar.setEnabled(indice< listaAgenda.size());
+
+
         /********************************/
         /* LO ANTERIOR EQUIVALE A ESTO: */
         /********************************/
@@ -98,7 +119,24 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class Persona{
+    public void borrarBtn(View view) {
+        if(borrable)
+            listaAgenda.remove(indice);
+
+        actualizaUI();
+    }
+
+    public void topIzdaBtn(View view) {
+        indice = 0;
+        actualizaUI();
+    }
+
+    public void topDchaBtn(View view) {
+        indice=listaAgenda.size();
+        actualizaUI();
+    }
+
+    private class Persona implements java.io.Serializable{
 
         public String nombre, apellido, telf;
 
@@ -107,6 +145,47 @@ public class MainActivity extends AppCompatActivity {
             this.apellido = apellido;
             this.telf = telf;
         }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Persona persona = (Persona) o;
+            return nombre.equals(persona.nombre) && apellido.equals(persona.apellido) && telf.equals(persona.telf);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(nombre, apellido, telf);
+        }
+    }
+
+    private class myTextChangedListener implements TextWatcher{
+
+        @Override
+        public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void afterTextChanged(Editable editable) {
+            if(indice<listaAgenda.size()) {
+                String textoActual0 = String.valueOf(nombre.getText());
+                String textoActual1 = String.valueOf(apellido.getText());
+                String textoActual2 = String.valueOf(telefono.getText());
+                guardar.setEnabled(!(borrable = listaAgenda.get(indice).equals(new Persona(textoActual0,textoActual1,textoActual2))));
+                borrar.setText(borrable?"Borrar":"Reset");
+            }
+        }
+    }
+
+    private class myLongClickIzda {
+        topI
     }
 }
 
